8ca0cf969d9d86e6fa62e305ef13711ef666f1e9
wikidesign <chi@chimeric.de>
2006-12-23 22:26:30 +0100

    32 Swedish language by Christer Nygren
    darcs-hash:20061223212630-89468-062e203daff9e5526aa7132bc8da4a34707d66e3.gz

---
+++ lang/se/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Swedish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Christer Nygren <wiki@fronet.fro.se>
+ */
+
+// for the configuration manager
+$lang['namespace']          = 'standard vy för blog';
+
+$lang['sortkey']            = 'sortera inlägg efter:';
+$lang['sortkey_o_cdate']    = 'skapat datum';
+$lang['sortkey_o_pagename'] = 'sidnamn';
+$lang['sortkey_o_id']       = 'sid ID';
+
+$lang['dateprefix']         = 'sätt datum före nytt inläggs ID';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
