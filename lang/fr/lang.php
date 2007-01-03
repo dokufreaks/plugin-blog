3b95fdfce362e27fe95253de44d70bcda08f8881
wikidesign <chi@chimeric.de>
2007-01-03 16:11:44 +0100

    33 French language update by Stanislas Reltgen
    darcs-hash:20070103151144-89468-06e6b23ef8a2953f5caf2b10fc0223407daf8367.gz

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -4,11 +4,13 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Damien Raude-Morvan <drazzib@drazzib.com>
+ * @author     Stanislas Reltgen <stanislas@reltgen.net>
  */
 
 // custom language strings for the plugin
+$lang['blog']     = 'Blog';
 $lang['older']    = 'Anciens billets';
-$lang['newer']    = 'Nouveaux billets';
+$lang['newer']    = 'Billets recents';
 $lang['newentry'] = 'Nouveau billet :';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -3,10 +3,17 @@
  * French language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Damien Raude-Morvan <drazzib@drazzib.com>
+ * @author     Stanislas Reltgen <stanislas@reltgen.net>
  */
  
 // for the configuration manager
-$lang['namespace']        = 'espace de nommage par defaut pour le blog';
+$lang['namespace']          = 'espace de nom par defaut pour le blog';
+
+$lang['sortkey']            = 'trier les billets du blog par:';
+$lang['sortkey_o_cdate']    = 'date de creation';
+$lang['sortkey_o_pagename'] = 'nom de la page';
+$lang['sortkey_o_id']       = 'ID de la page';
+
+$lang['dateprefix']         = 'date prefixant les IDs des nouveaux billets';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
