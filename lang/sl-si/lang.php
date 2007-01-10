eb4c460bcd22648be820110e7e8189ea956fd600
wikidesign <chi@chimeric.de>
2007-01-11 00:01:51 +0100

    37 Slovenian translation by Blaž Mertelj
    darcs-hash:20070110230151-89468-cc42901e7e5d361b0f87be682c2e12686f30e253.gz

---
+++ lang/sl-si/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Slovenian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Blaž Mertelj <Blaz.Mertelj@atol.si>
+ */
+
+// custom language strings for the plugin
+$lang['blog']       = 'Blog';
+$lang['older']      = 'Starejši vnosi'; 
+$lang['newer']      = 'Novejši vnosi'; 
+$lang['newentry']   = 'Nov blog vnos:'; 
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/sl-si/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Slovenian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Blaž Mertelj <Blaz.Mertelj@atol.si>
+ */
+ 
+// for the configuration manager
+$lang['namespace']          = 'privzeti namespace za blog';
+
+$lang['sortkey']            = 'sortiraj blog vnose po:';
+$lang['sortkey_o_cdate']    = 'ustvarjeno dne';
+$lang['sortkey_o_pagename'] = 'ime strani';
+$lang['sortkey_o_id']       = 'ID strani';
+
+$lang['dateprefix']         = 'pripeti datum pred IDje novih vnosov';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
