bb5fe0e80172cd4a9059f88b7bcdf23ef7e99897
raymond.scholz <chi@chimeric.de>
2007-04-28 01:53:14 +0200

    57 Add missing semicolon.
    darcs-hash:20070427235314-09e44-44f8ffa288c7968a25433be9635a57f582e75f50.gz

--- conf/default.php
+++ conf/default.php
@@ -2,10 +2,10 @@
 /**
  * Options for the Blog Plugin
  */
-$conf['namespace']    = 'blog';      // default location for blog entries
-$conf['formposition'] = 'bottom';    // position of new entry form
-$conf['dateprefix']   = '';          // prefix date to new entry IDs
-$conf['sortkey']      = 'cdate';     // sort key for blog entries
-$conf['sortorder']    = 'descending' // ascending or descending
+$conf['namespace']    = 'blog';       // default location for blog entries
+$conf['formposition'] = 'bottom';     // position of new entry form
+$conf['dateprefix']   = '';           // prefix date to new entry IDs
+$conf['sortkey']      = 'cdate';      // sort key for blog entries
+$conf['sortorder']    = 'descending'; // ascending or descending
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
