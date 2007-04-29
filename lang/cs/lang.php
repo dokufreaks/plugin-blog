bca976fb44283e1900b2c195c6e200f77a3b97e3
lukas.zapletal <chi@chimeric.de>
2007-04-29 23:46:04 +0200

    58 Czech translation
    darcs-hash:20070429214604-d7814-c25b82e54af8cde32427efe13bb833b8a6ce097c.gz

---
+++ lang/cs/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Czech language file (UTF-8 encoding)
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Lukas Zapletal <lukas.zapletal at gmail dot com>
+ */
+
+// custom language strings for the plugin
+$lang['blog']       = 'Blog';
+$lang['older']      = 'Starší zápisky';
+$lang['newer']      = 'Novější zápisky';
+$lang['newentry']   = 'Nový zápisek:';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/cs/settings.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Czech language file (UTF-8 encoding)
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Lukas Zapletal <lukas.zapletal at gmail dot com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']             = 'implicitní jmenný prostor pro blog';
+
+$lang['formposition']          = 'pozice pole pro zadání nového příspěvku';
+$lang['formposition_o_top']    = 'nahoře';
+$lang['formposition_o_bottom'] = 'dole';
+
+$lang['dateprefix']            = 'přidat datumový prefix novým příspěvkům';
+
+$lang['sortkey']               = 'razení zápisků podle';
+$lang['sortkey_o_cdate']       = 'data vytvoření';
+$lang['sortkey_o_mdate']       = 'data modifikace';
+$lang['sortkey_o_pagename']    = 'názvu stránky';
+$lang['sortkey_o_id']          = 'ID stránky';
+
+$lang['sortorder']              = 'pořadí';
+$lang['sortorder_o_ascending']  = 'vzestupné';
+$lang['sortorder_o_descending'] = 'sestupné';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
