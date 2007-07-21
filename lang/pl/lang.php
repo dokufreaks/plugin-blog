93641424274e69541d5fe64373deb8a9810ead58
wikidesign <chi@chimeric.de>
2007-07-21 12:03:30 +0200

    60 Polish translation by Patryk Ściborek
    darcs-hash:20070721100330-89468-03a561fce2f9b2fdcf08c935bcc8042cf0cb757b.gz

---
+++ lang/pl/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Polish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Patryk Ściborek <patryk@sciborek.com>
+ */
+
+// custom language strings for the plugin
+$lang['blog']       = 'Blog';
+$lang['older']      = 'Wcześniejsze wpisy';
+$lang['newer']      = 'Nowsze wpisy';
+$lang['newentry']   = 'Nowy wpis:';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/pl/settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Polish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Patryk Ściborek <patryk@sciborek.com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']             = 'domyślna przestrzeń nazw dla bloga';
+
+$lang['formposition']          = 'pozycja formularza nowy wpis';
+$lang['formposition_o_top']    = 'góra';
+$lang['formposition_o_bottom'] = 'dół';
+
+$lang['sortkey']               = 'sortuj wpisy używając:';
+$lang['sortkey_o_cdate']       = 'daty stworzenia';
+$lang['sortkey_o_pagename']    = 'nazwy strony';
+$lang['sortkey_o_id']          = 'ID strony';
+
+$lang['dateprefix']            = 'dołącz datę do ID nowego wpisu (ciąg formatujący datę)';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
