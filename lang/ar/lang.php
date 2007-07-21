d57a451bb01fcaf754c0c33d8dbbd5989cee7eff
wikidesign <chi@chimeric.de>
2007-07-21 12:27:08 +0200

    61 Arabic translation by M. Bashir Al-Noimi
    darcs-hash:20070721102708-89468-5585cf89d8617842b1e9fcb1e80d037b98dfb9fe.gz

---
+++ lang/ar/lang.php
@@ -0,0 +1,15 @@
+﻿<?php
+/**
+ * Arabic language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     M. Bashir Al-Noimi <bashir.storm@gmail.com>
+ */
+
+// custom language strings for the plugin
+$lang['blog']       = 'مدوّنة';
+$lang['older']      = 'تدوينات قديمة';
+$lang['newer']      = 'تدوينات جديدة';
+$lang['newentry']   = 'إضافة تدوينة جديدة:';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/ar/settings.php
@@ -0,0 +1,23 @@
+﻿<?php
+/**
+ * Arabic language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     M. Bashir Al-Noimi <bashir.storm@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']             = 'فضاء أسماء إفتراضي للمدوّنة';
+
+$lang['formposition']          = 'موقع التدوينة الجديدة';
+$lang['formposition_o_top']    = 'أعلى';
+$lang['formposition_o_bottom'] = 'أسفل';
+
+$lang['sortkey']               = 'ترتيب التدوينات حسب:';
+$lang['sortkey_o_cdate']       = 'تاريخ الإنشاء';
+$lang['sortkey_o_pagename']    = 'إسم الصفحة';
+$lang['sortkey_o_id']          = 'رقم الصفحة';
+
+$lang['dateprefix']            = 'بادئة التاريخ للتدوينات الجديدة';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
