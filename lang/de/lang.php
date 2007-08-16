7d0062b62ef41fd6fa282a033a3fd3b6d74627dd
wikidesign <chi@chimeric.de>
2007-08-16 17:35:48 +0200

    73 Fix for nasty bug in lang/de/lang.php
    darcs-hash:20070816153548-89468-fece2e163d6606b8e4dd508dcd3bdee0437b7544.gz

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -12,8 +12,8 @@ $lang['older']      = 'Ältere Einträge';
 $lang['newer']      = 'Neuere Einträge';
 $lang['newentry']   = 'Neuer Blogeintrag:';
 
-$lang]'missing_includeplugin'] = 'Ohne das Include Plugin funktioniert das Blog nicht.';
+$lang['missing_includeplugin']  = 'Ohne das Include Plugin funktioniert das Blog nicht.';
 $lang['missing_pagelistplugin'] = 'Ohne das Pagelist Plugin funktionieren Archivlisten nicht.';
-$lang['missing_tagplugin']     = 'Um Tag-Verfeinerungen zu verwenden, muss das Tag Plugin installiert sein.';
+$lang['missing_tagplugin']      = 'Um Tag-Verfeinerungen zu verwenden, muss das Tag Plugin installiert sein.';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
