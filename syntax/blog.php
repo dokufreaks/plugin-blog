8310833fa62db961b1f6efc4990ea3c0fd496e66
Gina Haeussge <osd@foosel.net>
2008-06-28 19:43:12 +0200

    enabled header permalink for blog posts
    The permalink flag is set by default to make the include plugin
    render the first header of included blog posts as a permalink.
    
    darcs-hash:20080628174312-2b4f5-c02c7f51deb8f57dcba0b855ceb26571ee3e1a3f.gz

--- syntax/blog.php
+++ syntax/blog.php
@@ -22,7 +22,7 @@ class syntax_plugin_blog_blog extends DokuWiki_Syntax_Plugin {
         return array(
                 'author' => 'Gina Häußge, Michael Klier, Esther Brunner',
                 'email'  => 'dokuwiki@chimeric.de',
-                'date'   => '2008-04-20',
+                'date'   => '2008-06-28',
                 'name'   => 'Blog Plugin (blog component)',
                 'desc'   => 'Displays a number of recent entries from a given namesspace',
                 'url'    => 'http://wiki.splitbrain.org/plugin:blog',
@@ -43,6 +43,7 @@ class syntax_plugin_blog_blog extends DokuWiki_Syntax_Plugin {
         $match = substr($match, 7, -2); // strip {{blog> from start and }} from end
         list($match, $flags) = explode('&', $match, 2);
         $flags =  explode('&', $flags);
+        $flags[] = 'permalink'; // always make the first header of a blog entry a permalink
         list($match, $refine) = explode(' ', $match, 2);
         list($ns, $num) = explode('?', $match, 2);
 
