848efc633e8cfab4382d551b74ef8f2e62edf598
Michael Klier <chi@chimeric.de>
2009-08-21 15:02:04 +0200

    updated polish translation

--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -3,21 +3,27 @@
  * Polish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Patryk Ściborek <patryk@sciborek.com>
+ * @author     Radoslaw Gorniak
  */
  
 // for the configuration manager
-$lang['namespace']             = 'domyślna przestrzeń nazw dla bloga';
+$lang['namespace']             = 'Domyślna przestrzeń nazw dla bloga';
 
-$lang['formposition']          = 'pozycja formularza nowy wpis';
+$lang['formposition']          = 'Pozycja formularza nowy wpis';
 $lang['formposition_o_top']    = 'góra';
 $lang['formposition_o_bottom'] = 'dół';
 
-$lang['sortkey']               = 'sortuj wpisy używając:';
+$lang['dateprefix']            = 'Dołącz datę do ID nowego wpisu (ciąg formatujący datę)';
+
+$lang['sortkey']               = 'Sortuj wpisy używając';
 $lang['sortkey_o_cdate']       = 'daty stworzenia';
+$lang['sortkey_o_mdate']       = 'daty modifikacji';
 $lang['sortkey_o_pagename']    = 'nazwy strony';
 $lang['sortkey_o_id']          = 'ID strony';
+$lang['sortkey_o_title']       = 'tytułu';
 
-$lang['dateprefix']            = 'dołącz datę do ID nowego wpisu (ciąg formatujący datę)';
+$lang['sortorder']              = 'Kolejność sortowania';
+$lang['sortorder_o_ascending']  = 'rosnąca';
+$lang['sortorder_o_descending'] = 'malejąca';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
