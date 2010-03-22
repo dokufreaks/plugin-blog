f37cff609aa41020a9eb8e61de19a682f9bab55e
Florent Chabaud <fchabaud@free.fr>
2010-03-22 12:18:05 +0100

    updated frensh translation

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,16 +4,36 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Stanislas Reltgen <stanislas@reltgen.net>
+ * @author     Florent Chabaud <fchabaud@free.fr>
  */
  
 // for the configuration manager
-$lang['namespace']          = 'espace de nom par defaut pour le blog';
+$lang['namespace']          	= 'espace de nom par defaut pour le blog';
 
-$lang['sortkey']            = 'trier les billets du blog par:';
-$lang['sortkey_o_cdate']    = 'date de creation';
-$lang['sortkey_o_pagename'] = 'nom de la page';
-$lang['sortkey_o_id']       = 'ID de la page';
+$lang['formposition']           = 'position du formulaire de saisie de nouveau billet';
+$lang['formposition_o_top']     = 'haut';
+$lang['formposition_o_bottom']  = 'bas';
 
-$lang['dateprefix']         = 'date prefixant les IDs des nouveaux billets';
+$lang['dateprefix']         	= 'date prefixant les IDs des nouveaux billets';
+
+$lang['sortkey']            	= 'trier les billets du blog par:';
+$lang['sortkey_o_cdate']    	= 'date de creation';
+$lang['sortkey_o_mdate']        = 'date de modification';
+$lang['sortkey_o_pagename'] 	= 'nom de la page';
+$lang['sortkey_o_id']       	= 'ID de la page';
+$lang['sortkey_o_title']        = 'titre';
+
+$lang['sortorder']              = 'ordre de tri';
+$lang['sortorder_o_ascending']  = 'croissant';
+$lang['sortorder_o_descending'] = 'décroissant';
+
+$lang['excluded_pages']         = 'exclure certaines pages des archives du blog (expression régulière requise)';
+
+$lang['showhistorgram']         = 'Affiche un histogramme sur les archives';
+$lang['max_months']             = 'Nombre maximal de mois à représenter dans l\'histogramme';
+$lang['histogram_height']       = 'Hauteur de l\'histogramme (en pixels)';
+
+
+
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
