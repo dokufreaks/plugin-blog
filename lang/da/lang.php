47bcd3208b7ee7461b97417538d414367b99849a
Rasmus Kinnerup <rasmus@kinnerup.com>
2010-03-29 12:02:22 +0200

    added Danish translation

---
+++ lang/da/lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Danish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Rasmus Kinnerup <rasmus@kinnerup.com>
+ */
+
+// custom language strings for the plugin
+$lang['blog']       = 'Blog';
+$lang['older']      = 'Ældre indlæg';
+$lang['newer']      = 'Nyere indlæg';
+$lang['newentry']   = 'Nyt indlæg:';
+
+$lang['missing_includeplugin']  = 'Include plugin skal være installeret for at bloggen virker.';
+$lang['missing_pagelistplugin'] = 'Pagelist plugin skal være installeret for at arkivlister kan virker.';
+$lang['missing_tagplugin']      = 'Tag plugin skal være installeret for at benytte denne funktion.';
+
+$lang['autoarchive'] = '%d indlæg i %s %s';
+$lang['month_01'] = 'januar';
+$lang['month_02'] = 'februar';
+$lang['month_03'] = 'marts';
+$lang['month_04'] = 'april';
+$lang['month_05'] = 'maj';
+$lang['month_06'] = 'juni';
+$lang['month_07'] = 'juli';
+$lang['month_08'] = 'august';
+$lang['month_09'] = 'september';
+$lang['month_10'] = 'oktober';
+$lang['month_11'] = 'november';
+$lang['month_12'] = 'december';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
