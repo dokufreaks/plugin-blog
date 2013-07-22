a02cb3712822a34d2b7cf3a1c7afe9052fb8b6a4
haobug <qingxianhao@gmail.com>
2013-07-22 14:57:00 +0800

    simplified chinese translation

---
+++ lang/zh/settings.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * English language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <wikidesign@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']              = '博客默认命名空间';
+
+$lang['formposition']           = '新文章的表单位置';
+$lang['formposition_o_top']     = '顶上';
+$lang['formposition_o_bottom']  = '底下';
+$lang['formposition_o_none']    = '不显示';
+
+$lang['newentrytitle']          = '新文章表单的标题';
+
+$lang['dateprefix']             = '为新文章 ID 添加日期前缀';
+
+$lang['sortkey']                = '文章排序按';
+$lang['sortkey_o_cdate']        = '创建时间';
+$lang['sortkey_o_mdate']        = '修改时间';
+$lang['sortkey_o_pagename']     = '页面名称';
+$lang['sortkey_o_id']           = '页面 ID';
+$lang['sortkey_o_title']        = '标题';
+
+$lang['sortorder']              = '排序顺序';
+$lang['sortorder_o_ascending']  = '升序';
+$lang['sortorder_o_descending'] = '降序';
+
+$lang['excluded_pages']         = '从博客列表中排除特定的页面 (要求是正则表达式)';
+
+$lang['showhistogram']          = '在存档页面上显示直方图';
+$lang['max_months']             = '在直方图上最多显示月份数';
+$lang['histogram_height']       = '直方图的高度 (单位像素)';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
