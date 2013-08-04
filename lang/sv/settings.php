554c5dc6db8a3ecbf1efe016b0d6651afa59c195
Tor Härnqvist <tor.harnqvist@gmail.com>
2013-08-04 22:50:06 +0200

    translation update

--- lang/sv/settings.php
+++ lang/sv/settings.php
@@ -1,19 +1,23 @@
 <?php
+
 /**
- * Swedish language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Christer Nygren <wiki@fronet.fro.se>
+ * @author Tor Härnqvist <tor.harnqvist@gmail.com>
  */
-
-// for the configuration manager
-$lang['namespace']          = 'standard vy för blog';
-
-$lang['sortkey']            = 'sortera inlägg efter:';
-$lang['sortkey_o_cdate']    = 'skapat datum';
-$lang['sortkey_o_pagename'] = 'sidnamn';
-$lang['sortkey_o_id']       = 'sid ID';
-
-$lang['dateprefix']         = 'sätt datum före nytt inläggs ID';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['namespace']             = 'standard vy för blogg';
+$lang['formposition_o_none']   = 'inget';
+$lang['newentrytitle']         = 'rubrik för nytt inläggsformulär';
+$lang['dateprefix']            = 'sätt datum före nytt inläggs-ID';
+$lang['sortkey']               = 'sortera inlägg efter:';
+$lang['sortkey_o_cdate']       = 'skapat datum';
+$lang['sortkey_o_mdate']       = 'ändringsdatum';
+$lang['sortkey_o_pagename']    = 'sidnamn';
+$lang['sortkey_o_id']          = 'sid-ID';
+$lang['sortkey_o_title']       = 'rubrik';
+$lang['sortorder']             = 'sorteringsordning';
+$lang['sortorder_o_ascending'] = 'stigande';
+$lang['sortorder_o_descending'] = 'fallande';
+$lang['max_months']            = 'Maximalt antal månader att visa i histogramet';
+$lang['histogram_height']      = 'Histogramets höjd (i pixlar)';
