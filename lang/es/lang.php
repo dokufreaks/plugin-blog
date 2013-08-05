2e5d35a483f8fe8a62762531567d72084c83d4b9
r0sk <r0sk10@gmail.com>
2013-08-06 00:45:05 +0200

    translation update

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -1,14 +1,30 @@
 <?php
+
 /**
- * Archivo en español
- *
- * @licence    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Herman Fabián Sandoval Manrique <hfsandovalm@emzac.com>
+ * @author r0sk <r0sk10@gmail.com>
  */
-
-// frases personalizadas en español para el plugin
-$lang['older']      = 'Entradas anteriores';
-$lang['newer']      = 'Entradas nuevas';
-$lang['newentry']   = 'Nueva entrada en el blog:';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['blog']                  = 'Blog';
+$lang['older']                 = 'Entradas anteriores';
+$lang['newer']                 = 'Entradas nuevas';
+$lang['newentry']              = 'Nueva entrada en el blog:';
+$lang['missing_includeplugin'] = 'El plugin Include debe estar instalado para que el blog funcione.';
+$lang['missing_pagelistplugin'] = 'El plugin Pagelist debe estar instalado para que funcionen los listados de archivo.';
+$lang['missing_tagplugin']     = 'El plugin Tag debe estar instalado para que funcionen los tags.';
+$lang['month_01']              = 'Enero';
+$lang['month_02']              = 'Febrero';
+$lang['month_03']              = 'Marzo';
+$lang['month_04']              = 'Abril';
+$lang['month_05']              = 'Mayo';
+$lang['month_06']              = 'Junio';
+$lang['month_07']              = 'Julio';
+$lang['month_08']              = 'Agosto';
+$lang['month_09']              = 'Septiembre';
+$lang['month_10']              = 'Octubre';
+$lang['month_11']              = 'Noviembre';
+$lang['month_12']              = 'Diciembre';
+$lang['entries']               = 'entradas';
+$lang['entry']                 = 'entrada';
+$lang['archive_title']         = 'Histórico';
