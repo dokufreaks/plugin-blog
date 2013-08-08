6c17ad87f5999597d4b44a4fd2ca2ac07b15d23d
Mark Prins <mprins@users.sf.net>
2013-08-08 17:35:05 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -1,14 +1,31 @@
 <?php
+
 /**
- * dutch language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Gijs H. van Gemert <g.v.gemert@inter.nl.net>
+ * @author Mark Prins <mprins@users.sf.net>
  */
-
-// custom language strings for the plugin
-$lang['older']      = 'Oude berichten';
-$lang['newer']      = 'Nieuwe berichten';
-$lang['newentry']   = 'Nieuw bericht:';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['blog']                  = 'Blog';
+$lang['older']                 = 'Oude berichten';
+$lang['newer']                 = 'Nieuwe berichten';
+$lang['newentry']              = 'Nieuw bericht:';
+$lang['missing_includeplugin'] = 'De Include Plugin dient geïnstalleerd te zijn om de blog te laten werken.';
+$lang['missing_pagelistplugin'] = 'De Pagelist Plugin dient geïnstalleerd te zijn om archief lijsten te laten werken.';
+$lang['missing_tagplugin']     = 'De Tag Plugin dient geïnstalleerd te zijn om label filters te laten werken.';
+$lang['autoarchive']           = '%d artikel(en) voor %s %s';
+$lang['month_01']              = 'januari';
+$lang['month_02']              = 'februari';
+$lang['month_03']              = 'maart';
+$lang['month_04']              = 'april';
+$lang['month_05']              = 'mei';
+$lang['month_06']              = 'juni';
+$lang['month_07']              = 'juli';
+$lang['month_08']              = 'augustus';
+$lang['month_09']              = 'september';
+$lang['month_10']              = 'october';
+$lang['month_11']              = 'november';
+$lang['month_12']              = 'december';
+$lang['entries']               = 'berichten';
+$lang['entry']                 = 'bericht';
+$lang['archive_title']         = 'Blog geschiedenis';
