adc6d545024ba3ef86538e9a3199766ec93a9ade
Myeongjin <aranet100@gmail.com>
2013-08-13 15:45:05 +0200

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['blog']                  = '블로그';
+$lang['older']                 = '오래된 항목';
+$lang['newer']                 = '새 항목';
+$lang['newentry']              = '새 블로그 항목:';
+$lang['missing_includeplugin'] = '포함된 플러그인은 블로그가 작동하기 위해 설치되어야 합니다.';
+$lang['missing_pagelistplugin'] = 'Pagelist 플러그인은 보존 목록이 작동하기 위해 설치되어야 합니다.';
+$lang['missing_tagplugin']     = 'Tag 플러그인은 태그 분류를 사용하기 위해 설치되어야 합니다.';
+$lang['autoarchive']           = '게시물 %d개, %s %s년';
+$lang['month_01']              = '1월';
+$lang['month_02']              = '2월';
+$lang['month_03']              = '3월';
+$lang['month_04']              = '4월';
+$lang['month_05']              = '5월';
+$lang['month_06']              = '6월';
+$lang['month_07']              = '7월';
+$lang['month_08']              = '8월';
+$lang['month_09']              = '9월';
+$lang['month_10']              = '10월';
+$lang['month_11']              = '11월';
+$lang['month_12']              = '12월';
+$lang['entries']               = '항목';
+$lang['entry']                 = '항목';
+$lang['archive_title']         = '블로그 역사';
