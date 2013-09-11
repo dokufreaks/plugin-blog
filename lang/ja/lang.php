7891ba9eb476d6b600ad5aba91e9a8b8d1e2058b
Hideaki SAWADA <chuno@live.jp>
2013-09-11 17:35:05 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['blog']                  = 'ブログ';
+$lang['older']                 = '古い記事';
+$lang['newer']                 = '新しい記事';
+$lang['newentry']              = '新しい記事の投稿：';
+$lang['missing_includeplugin'] = 'blog が動くのに Include プラグインが必要';
+$lang['missing_pagelistplugin'] = 'archive lists が動くのに Pagelist プラグインが必要';
+$lang['missing_tagplugin']     = 'tag refinements を使用するのに Tag Plugin プラグインが必要';
+$lang['entries']               = '記事';
+$lang['entry']                 = '記事';
+$lang['archive_title']         = 'ブログ履歴';
