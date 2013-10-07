9770915d1f4dee788b20a3673f97f07c014e4e04
Serenity87HUN <anikototh87@gmail.com>
2013-10-07 15:45:06 +0200

    translation update

--- lang/hu/settings.php
+++ lang/hu/settings.php
@@ -1,12 +1,26 @@
 <?php
-/**
- * Hungarian language file
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Norbert Csík <norbert.csik@gmail.com>
- */
- 
-// for the configuration manager
-$lang['namespace']        = 'blog alapértelmezett névtere';
 
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author     Norbert Csík <norbert.csik@gmail.com>
+ * @author Serenity87HUN <anikototh87@gmail.com>
+ */
+$lang['namespace']             = 'blog alapértelmezett névtere';
+$lang['formposition']          = 'új bejegyzés űrlapjának helye';
+$lang['formposition_o_top']    = 'felül';
+$lang['formposition_o_bottom'] = 'alul';
+$lang['formposition_o_none']   = 'sehol';
+$lang['newentrytitle']         = 'új bejegyzés címének űrlapja';
+$lang['sortkey']               = 'bejegyzések rendezésének feltétele';
+$lang['sortkey_o_cdate']       = 'készítési dátum';
+$lang['sortkey_o_mdate']       = 'módosítási dátum';
+$lang['sortkey_o_pagename']    = 'oldal neve';
+$lang['sortkey_o_id']          = 'oldal azonosítója';
+$lang['sortkey_o_title']       = 'cím';
+$lang['sortorder']             = 'rendezés';
+$lang['sortorder_o_ascending'] = 'növekvő';
+$lang['sortorder_o_descending'] = 'csökkenő';
+$lang['excluded_pages']        = 'bizonyos oldalak kihagyása a blog listázása esetén (regurális kifejezés használata szükséges)';
+$lang['max_months']            = 'Histogram-on szereplő hónapok maximális száma';
+$lang['histogram_height']      = 'Histogram magassága (pixel)';
