9fad3f166462d47210b7d98491afadaf51141c9d
Soren Birk <soer9648@eucl.dk>
2013-11-12 20:56:51 +0100

    translation update

--- lang/da/lang.php
+++ lang/da/lang.php
@@ -1,34 +1,31 @@
-﻿<?php
+<?php
+
 /**
- * Danish language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
+ * 
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Soren Birk <soer9648@eucl.dk>
  */
-
-// custom language strings for the plugin
-$lang['blog']       = 'Blog';
-$lang['older']      = 'Ældre indlæg';
-$lang['newer']      = 'Nyere indlæg';
-$lang['newentry']   = 'Nyt indlæg:';
-
-$lang['missing_includeplugin']  = 'Include plugin skal være installeret for at bloggen virker.';
+$lang['blog']                  = 'Blog';
+$lang['older']                 = 'Ældre indlæg';
+$lang['newer']                 = 'Nyere indlæg';
+$lang['newentry']              = 'Nyt indlæg:';
+$lang['missing_includeplugin'] = 'Include plugin skal være installeret for at bloggen virker.';
 $lang['missing_pagelistplugin'] = 'Pagelist plugin skal være installeret for at arkivlister virker.';
-$lang['missing_tagplugin']      = 'Tag plugin skal være installeret for at benytte denne funktion.';
-
-$lang['autoarchive'] = '%1$d indlæg i %2$s %3$s';
-$lang['month_01'] = 'januar';
-$lang['month_02'] = 'februar';
-$lang['month_03'] = 'marts';
-$lang['month_04'] = 'april';
-$lang['month_05'] = 'maj';
-$lang['month_06'] = 'juni';
-$lang['month_07'] = 'juli';
-$lang['month_08'] = 'august';
-$lang['month_09'] = 'september';
-$lang['month_10'] = 'oktober';
-$lang['month_11'] = 'november';
-$lang['month_12'] = 'december';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
- 
+$lang['missing_tagplugin']     = 'Tag plugin skal være installeret for at benytte denne funktion.';
+$lang['autoarchive']           = '%1$d indlæg i %2$s %3$s';
+$lang['month_01']              = 'januar';
+$lang['month_02']              = 'februar';
+$lang['month_03']              = 'marts';
+$lang['month_04']              = 'april';
+$lang['month_05']              = 'maj';
+$lang['month_06']              = 'juni';
+$lang['month_07']              = 'juli';
+$lang['month_08']              = 'august';
+$lang['month_09']              = 'september';
+$lang['month_10']              = 'oktober';
+$lang['month_11']              = 'november';
+$lang['month_12']              = 'december';
+$lang['entries']               = 'indlæg';
+$lang['entry']                 = 'indlæg';
+$lang['archive_title']         = 'Blog Arkiv';
--- lang/da/settings.php
+++ lang/da/settings.php
@@ -1,31 +1,24 @@
-﻿<?php
+<?php
+
 /**
- * Danish language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
+ * 
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Soren Birk <soer9648@eucl.dk>
  */
- 
-// for the configuration manager
-$lang['namespace']              = 'standard navnerum til blog';
-
-$lang['formposition']           = 'position af formular til nyt indlæg';
-$lang['formposition_o_top']     = 'top';
-$lang['formposition_o_bottom']  = 'bund';
-
-$lang['dateprefix']             = 'foruddefineret dato for nye indlæg IDs';
-
-$lang['sortkey']                = 'sortér blog indlæg efter';
-$lang['sortkey_o_cdate']        = 'dato for oprettelse';
-$lang['sortkey_o_mdate']        = 'dato for seneste ændring';
-$lang['sortkey_o_pagename']     = 'sidenavn';
-$lang['sortkey_o_id']           = 'side id';
-$lang['sortkey_o_title']        = 'titel';
-
-$lang['sortorder']              = 'sorteringsrækkefølge';
-$lang['sortorder_o_ascending']  = 'stigende';
+$lang['namespace']             = 'standard navnerum til blog';
+$lang['formposition']          = 'position af formular til nyt indlæg';
+$lang['formposition_o_top']    = 'top';
+$lang['formposition_o_bottom'] = 'bund';
+$lang['formposition_o_none']   = 'intet';
+$lang['dateprefix']            = 'foruddefineret dato for nye indlæg IDs';
+$lang['sortkey']               = 'sortér blog indlæg efter';
+$lang['sortkey_o_cdate']       = 'dato for oprettelse';
+$lang['sortkey_o_mdate']       = 'dato for seneste ændring';
+$lang['sortkey_o_pagename']    = 'sidenavn';
+$lang['sortkey_o_id']          = 'side id';
+$lang['sortkey_o_title']       = 'titel';
+$lang['sortorder']             = 'sorteringsrækkefølge';
+$lang['sortorder_o_ascending'] = 'stigende';
 $lang['sortorder_o_descending'] = 'faldende';
-
-$lang['excluded_pages']         = 'Udeluk bestemte sider fra blog listen (kræver regulært udtryk)';
-
-// vim:ts=4:sw=4:et:enc=utf-8:
+$lang['excluded_pages']        = 'Udeluk bestemte sider fra blog listen (kræver regulært udtryk)';
