3073203498015046fdcbdaf18ec0f5884f9a7a7a
Nicolas Friedli <nicolas@theologique.ch>
2013-12-17 14:30:05 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,39 +1,32 @@
 <?php
+
 /**
- * French language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Damien Raude-Morvan <drazzib@drazzib.com>
- * @author     Stanislas Reltgen <stanislas@reltgen.net>
- * @author     Florent Chabaud <fchabaud@free.fr>
+ * 
+ * @author Damien Raude-Morvan <drazzib@drazzib.com>
+ * @author Stanislas Reltgen <stanislas@reltgen.net>
+ * @author Florent Chabaud <fchabaud@free.fr>
  */
-
-// custom language strings for the plugin
-$lang['blog']     = 'Blog';
-$lang['older']    = 'Anciens billets';
-$lang['newer']    = 'Billets recents';
-$lang['newentry'] = 'Nouveau billet :';
-
-$lang['missing_includeplugin']  = 'Le module "include" doit être installé pour que le module "blog" puisse fonctionner.';
+$lang['blog']                  = 'Blog';
+$lang['older']                 = 'Anciens billets';
+$lang['newer']                 = 'Billets recents';
+$lang['newentry']              = 'Nouveau billet :';
+$lang['missing_includeplugin'] = 'Le module "include" doit être installé pour que le module "blog" puisse fonctionner.';
 $lang['missing_pagelistplugin'] = 'Le module "pagelist" doit être installé pour que les archives puissent fonctionner.';
-$lang['missing_tagplugin']      = 'Le module "tag" doit être installé pour pouvoir en bénéficier dans le module "blog".';
-
-$lang['autoarchive'] = '%1$d billet(s) pour %2$s %3$s';
-$lang['month_01'] = 'janvier';
-$lang['month_02'] = 'février';
-$lang['month_03'] = 'mars';
-$lang['month_04'] = 'avril';
-$lang['month_05'] = 'mai';
-$lang['month_06'] = 'juin';
-$lang['month_07'] = 'juillet';
-$lang['month_08'] = 'août';
-$lang['month_09'] = 'septembre';
-$lang['month_10'] = 'octobre';
-$lang['month_11'] = 'novembre';
-$lang['month_12'] = 'décembre';
-
-$lang['entries']       = 'billets';
-$lang['entry']         = 'billet';
-$lang['archive_title'] = 'Historique du blog';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['missing_tagplugin']     = 'Le module "tag" doit être installé pour pouvoir en bénéficier dans le module "blog".';
+$lang['autoarchive']           = '%1$d billet(s) pour %2$s %3$s';
+$lang['month_01']              = 'janvier';
+$lang['month_02']              = 'février';
+$lang['month_03']              = 'mars';
+$lang['month_04']              = 'avril';
+$lang['month_05']              = 'mai';
+$lang['month_06']              = 'juin';
+$lang['month_07']              = 'juillet';
+$lang['month_08']              = 'août';
+$lang['month_09']              = 'septembre';
+$lang['month_10']              = 'octobre';
+$lang['month_11']              = 'novembre';
+$lang['month_12']              = 'décembre';
+$lang['entries']               = 'billets';
+$lang['entry']                 = 'billet';
+$lang['archive_title']         = 'Historique du blog';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,39 +1,29 @@
 <?php
+
 /**
- * French language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Stanislas Reltgen <stanislas@reltgen.net>
- * @author     Florent Chabaud <fchabaud@free.fr>
+ * 
+ * @author Stanislas Reltgen <stanislas@reltgen.net>
+ * @author Florent Chabaud <fchabaud@free.fr>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
- 
-// for the configuration manager
-$lang['namespace']          	= 'espace de nom par defaut pour le blog';
-
-$lang['formposition']           = 'position du formulaire de saisie de nouveau billet';
-$lang['formposition_o_top']     = 'haut';
-$lang['formposition_o_bottom']  = 'bas';
-
-$lang['dateprefix']         	= 'date prefixant les IDs des nouveaux billets';
-
-$lang['sortkey']            	= 'trier les billets du blog par:';
-$lang['sortkey_o_cdate']    	= 'date de creation';
-$lang['sortkey_o_mdate']        = 'date de modification';
-$lang['sortkey_o_pagename'] 	= 'nom de la page';
-$lang['sortkey_o_id']       	= 'ID de la page';
-$lang['sortkey_o_title']        = 'titre';
-
-$lang['sortorder']              = 'ordre de tri';
-$lang['sortorder_o_ascending']  = 'croissant';
+$lang['namespace']             = 'espace de nom par defaut pour le blog';
+$lang['formposition']          = 'position du formulaire de saisie de nouveau billet';
+$lang['formposition_o_top']    = 'haut';
+$lang['formposition_o_bottom'] = 'bas';
+$lang['formposition_o_none']   = 'aucun';
+$lang['newentrytitle']         = 'titre de la nouvelle entrée de formulaire';
+$lang['dateprefix']            = 'date prefixant les IDs des nouveaux billets';
+$lang['sortkey']               = 'trier les billets du blog par:';
+$lang['sortkey_o_cdate']       = 'date de creation';
+$lang['sortkey_o_mdate']       = 'date de modification';
+$lang['sortkey_o_pagename']    = 'nom de la page';
+$lang['sortkey_o_id']          = 'ID de la page';
+$lang['sortkey_o_title']       = 'titre';
+$lang['sortorder']             = 'ordre de tri';
+$lang['sortorder_o_ascending'] = 'croissant';
 $lang['sortorder_o_descending'] = 'décroissant';
-
-$lang['excluded_pages']         = 'exclure certaines pages des archives du blog (expression régulière requise)';
-
-$lang['showhistorgram']         = 'Affiche un histogramme sur les archives';
-$lang['max_months']             = 'Nombre maximal de mois à représenter dans l\'histogramme';
-$lang['histogram_height']       = 'Hauteur de l\'histogramme (en pixels)';
-
-
-
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['excluded_pages']        = 'exclure certaines pages des archives du blog (expression régulière requise)';
+$lang['showhistogram']         = 'Afficher l\'histogramme du rendu des archives';
+$lang['max_months']            = 'Nombre maximal de mois à représenter dans l\'histogramme';
+$lang['histogram_height']      = 'Hauteur de l\'histogramme (en pixels)';
