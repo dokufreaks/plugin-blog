aeade7d8d27ddc2e36f20390683343409571a727
Marina Vladi <deldadam@gmail.com>
2014-04-26 21:30:33 +0200

    translation update

--- lang/hu/lang.php
+++ lang/hu/lang.php
@@ -3,13 +3,18 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
- * @author     Norbert Csík <norbert.csik@gmail.com>
+ * @author Norbert Csík <norbert.csik@gmail.com>
  * @author Serenity87HUN <anikototh87@gmail.com>
+ * @author Marina Vladi <deldadam@gmail.com>
  */
-$lang['blog']                  = 'Blog';
+$lang['blog']                  = 'Napló';
 $lang['older']                 = 'régi bejegyzések';
 $lang['newer']                 = 'új bejegyzések';
 $lang['newentry']              = 'Új bejegyzés:';
+$lang['missing_includeplugin'] = 'Az \'Include Plugin\'-t telepítenünk kell a napló (blog) működéséhez.';
+$lang['missing_pagelistplugin'] = 'A \'Pagelist Plugin\'-t telepítenünk kell az archiválási listák működéséhez.';
+$lang['missing_tagplugin']     = 'A \'Tag Plugin\'-t telepítenünk kell a címkék használatához.';
+$lang['autoarchive']           = '%1$d hozzászólás ehhez: %2$s %3$s';
 $lang['month_01']              = 'Január';
 $lang['month_02']              = 'Február';
 $lang['month_03']              = 'Március';
@@ -22,6 +27,6 @@ $lang['month_09']              = 'Szeptember';
 $lang['month_10']              = 'Október';
 $lang['month_11']              = 'November';
 $lang['month_12']              = 'December';
-$lang['entries']               = 'bejegyzések';
+$lang['entries']               = 'bejegyzés';
 $lang['entry']                 = 'bejegyzés';
-$lang['archive_title']         = 'Blog története';
+$lang['archive_title']         = 'Naplótörténet';
--- lang/hu/settings.php
+++ lang/hu/settings.php
@@ -3,24 +3,27 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
- * @author     Norbert Csík <norbert.csik@gmail.com>
+ * @author Norbert Csík <norbert.csik@gmail.com>
  * @author Serenity87HUN <anikototh87@gmail.com>
+ * @author Marina Vladi <deldadam@gmail.com>
  */
-$lang['namespace']             = 'blog alapértelmezett névtere';
+$lang['namespace']             = 'napló alapértelmezett névtere';
 $lang['formposition']          = 'új bejegyzés űrlapjának helye';
 $lang['formposition_o_top']    = 'felül';
 $lang['formposition_o_bottom'] = 'alul';
 $lang['formposition_o_none']   = 'sehol';
-$lang['newentrytitle']         = 'új bejegyzés címének űrlapja';
-$lang['sortkey']               = 'bejegyzések rendezésének feltétele';
-$lang['sortkey_o_cdate']       = 'készítési dátum';
-$lang['sortkey_o_mdate']       = 'módosítási dátum';
-$lang['sortkey_o_pagename']    = 'oldal neve';
-$lang['sortkey_o_id']          = 'oldal azonosítója';
+$lang['newentrytitle']         = 'új bejegyzés űrlapcíme';
+$lang['dateprefix']            = 'Dátumelőtag új bejegyzések ID-jéhez';
+$lang['sortkey']               = 'naplóbejegyzések rendezése eszerint';
+$lang['sortkey_o_cdate']       = 'készítés dátuma';
+$lang['sortkey_o_mdate']       = 'módosítás dátuma';
+$lang['sortkey_o_pagename']    = 'oldalnév';
+$lang['sortkey_o_id']          = 'oldalazonosító';
 $lang['sortkey_o_title']       = 'cím';
-$lang['sortorder']             = 'rendezés';
+$lang['sortorder']             = 'rendezés iránya';
 $lang['sortorder_o_ascending'] = 'növekvő';
 $lang['sortorder_o_descending'] = 'csökkenő';
 $lang['excluded_pages']        = 'bizonyos oldalak kihagyása a blog listázása esetén (regurális kifejezés használata szükséges)';
-$lang['max_months']            = 'Histogram-on szereplő hónapok maximális száma';
-$lang['histogram_height']      = 'Histogram magassága (pixel)';
+$lang['showhistogram']         = 'Grafikon megjelenítése archiváláskor';
+$lang['max_months']            = 'Grafikonon megjelenítendő hónapok max. száma';
+$lang['histogram_height']      = 'Grafikon magassága (pixelben)';
