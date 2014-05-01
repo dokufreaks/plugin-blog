64e38a14c2cf93c77b67eec47118ee4e705b4539
Fernando Ribeiro <pinguim.ribeiro@gmail.com>
2014-05-01 03:02:13 +0100

    added Portuguese language files

---
+++ lang/pt/lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Portuguese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <wikidesign@gmail.com>
+ * @author     Fernando Ribeiro <pinguim.ribeiro@gmail.com>
+ */
+
+// custom language strings for the plugin
+$lang['blog']       = 'Blog';
+$lang['older']      = 'Entradas antigas';
+$lang['newer']      = 'Entradas recentes';
+$lang['newentry']   = 'Nova entrada do blog:';
+
+$lang['missing_includeplugin']  = 'O Plugin Include deve estar instalado para o blog funcionar.';
+$lang['missing_pagelistplugin'] = 'O Plugin Pagelist deve estar instalado para as listas do arquivo funcionarem.';
+$lang['missing_tagplugin']      = 'O Plugin Tag deve estar instalado para as etiquetas funcionarem.';
+
+$lang['autoarchive'] = '%1$d Artigo(s) para %2$s %3$s';
+$lang['month_01'] = 'Janeiro';
+$lang['month_02'] = 'Fevereiro';
+$lang['month_03'] = 'Março';
+$lang['month_04'] = 'Abril';
+$lang['month_05'] = 'Maio';
+$lang['month_06'] = 'Junho';
+$lang['month_07'] = 'Julho';
+$lang['month_08'] = 'Agosto';
+$lang['month_09'] = 'Setembro';
+$lang['month_10'] = 'Outubro';
+$lang['month_11'] = 'Novembro';
+$lang['month_12'] = 'Dezembro';
+
+$lang['entries']       = 'entradas';
+$lang['entry']         = 'entrada';
+$lang['archive_title'] = 'Histórico do Blog';
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/pt/settings.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Portuguese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <wikidesign@gmail.com>
+ * @author     Fernando Ribeiro <pinguim.ribeiro@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']              = 'default namespace for blog';
+
+$lang['formposition']           = 'position of the new entry form';
+$lang['formposition_o_top']     = 'top';
+$lang['formposition_o_bottom']  = 'bottom';
+$lang['formposition_o_none']    = 'none';
+
+$lang['newentrytitle']          = 'title of the new entry form';
+
+$lang['dateprefix']             = 'prefix date to new entry IDs';
+
+$lang['sortkey']                = 'sort blog entries by';
+$lang['sortkey_o_cdate']        = 'data de criação';
+$lang['sortkey_o_mdate']        = 'data de modificação';
+$lang['sortkey_o_pagename']     = 'nome da página';
+$lang['sortkey_o_id']           = 'ID da página';
+$lang['sortkey_o_title']        = 'título';
+
+$lang['sortorder']              = 'ordenação';
+$lang['sortorder_o_ascending']  = 'ascendente';
+$lang['sortorder_o_descending'] = 'descendente';
+
+$lang['excluded_pages']         = 'escluir determinadas páginas da listagem do blog (requer expressão regular)';
+
+$lang['showhistogram']          = 'Display histogram on archive rendering';
+$lang['max_months']             = 'Max months to show in the histogram';
+$lang['histogram_height']       = 'Height of the histogram (in pixels)';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
