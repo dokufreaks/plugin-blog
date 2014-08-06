c8fd808642ae6054242483741f6c2d68a36d4580
Tomasz Dworakowski <tomaszdworakowski@gmail.com>
2014-08-06 13:05:26 +0200

    translation update

--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -4,11 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Radoslaw Gorniak
+ * @author Tomasz Dworakowski <tomaszdworakowski@gmail.com>
  */
 $lang['namespace']             = 'Domyślna przestrzeń nazw dla bloga';
 $lang['formposition']          = 'Pozycja formularza nowy wpis';
 $lang['formposition_o_top']    = 'góra';
 $lang['formposition_o_bottom'] = 'dół';
+$lang['formposition_o_none']   = 'brak';
 $lang['dateprefix']            = 'Dołącz datę do ID nowego wpisu (ciąg formatujący datę)';
 $lang['sortkey']               = 'Sortuj wpisy używając';
 $lang['sortkey_o_cdate']       = 'daty stworzenia';
