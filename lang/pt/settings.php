7d438e30e7b16bb8a7070a56a69b80ad7dd777c4
vidajerusha <vidajerusha@msn.com>
2014-10-12 19:45:05 +0200

    translation update

--- lang/pt/settings.php
+++ lang/pt/settings.php
@@ -1,39 +1,28 @@
 <?php
+
 /**
- * Portuguese language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     Fernando Ribeiro <pinguim.ribeiro@gmail.com>
+ * 
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Fernando Ribeiro <pinguim.ribeiro@gmail.com>
  */
- 
-// for the configuration manager
-$lang['namespace']              = 'default namespace for blog';
-
-$lang['formposition']           = 'posição do formulário de entrada nova';
-$lang['formposition_o_top']     = 'topo';
-$lang['formposition_o_bottom']  = 'fundo';
-$lang['formposition_o_none']    = 'nenhum';
-
-$lang['newentrytitle']          = 'título do formulário de entrada nova';
-
-$lang['dateprefix']             = 'prefixo do ID para a nova entrada';
-
-$lang['sortkey']                = 'ordenar entradas do blog por';
-$lang['sortkey_o_cdate']        = 'data de criação';
-$lang['sortkey_o_mdate']        = 'data de modificação';
-$lang['sortkey_o_pagename']     = 'nome da página';
-$lang['sortkey_o_id']           = 'ID da página';
-$lang['sortkey_o_title']        = 'título';
-
-$lang['sortorder']              = 'ordenação';
-$lang['sortorder_o_ascending']  = 'ascendente';
+$lang['namespace']             = 'default namespace for blog';
+$lang['formposition']          = 'posição do formulário de entrada nova';
+$lang['formposition_o_top']    = 'topo';
+$lang['formposition_o_bottom'] = 'fundo';
+$lang['formposition_o_none']   = 'nenhum';
+$lang['newentrytitle']         = 'título do formulário de entrada nova';
+$lang['dateprefix']            = 'prefixo do ID para a nova entrada';
+$lang['sortkey']               = 'ordenar entradas do blog por';
+$lang['sortkey_o_cdate']       = 'data de criação';
+$lang['sortkey_o_mdate']       = 'data de modificação';
+$lang['sortkey_o_pagename']    = 'nome da página';
+$lang['sortkey_o_id']          = 'ID da página';
+$lang['sortkey_o_title']       = 'título';
+$lang['sortorder']             = 'ordenação';
+$lang['sortorder_o_ascending'] = 'ascendente';
 $lang['sortorder_o_descending'] = 'descendente';
-
-$lang['excluded_pages']         = 'excluir determinadas páginas da listagem do blog (requer expressão regular)';
-
-$lang['showhistogram']          = 'Mostrar histograma na representação do arquivo';
-$lang['max_months']             = 'Nº máximo de meses para mostrar no histograma';
-$lang['histogram_height']       = 'Altura do histograma (em pixels)';
-
-// vim:ts=4:sw=4:et:enc=utf-8:
+$lang['excluded_pages']        = 'excluir determinadas páginas da listagem do blog (requer expressão regular)';
+$lang['showhistogram']         = 'Mostrar histograma na representação do arquivo';
+$lang['max_months']            = 'Nº máximo de meses para mostrar no histograma';
+$lang['histogram_height']      = 'Altura do histograma (em pixels)';
