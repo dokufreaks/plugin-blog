34b0c5dee41d8c985650c534f38ebef0f181723e
Otto Vainio <otto@valjakko.net>
2015-04-14 17:46:08 +0300

    translation for fi

---
+++ lang/fi/settings.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * English language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <wikidesign@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']              = 'default namespace for blog';
+
+$lang['formposition']           = 'position of the new entry form';
+$lang['formposition_o_top']     = 'top';
+$lang['formposition_o_bottom']  = 'bottom';
+$lang['formposition_o_none']    = 'none';
+
+$lang['newentrytitle']          = 'title of the new entry form';
+
+$lang['dateprefix']             = 'prefix date to new entry IDs';
+
+$lang['sortkey']                = 'sort blog entries by';
+$lang['sortkey_o_cdate']        = 'creation date';
+$lang['sortkey_o_mdate']        = 'modification date';
+$lang['sortkey_o_pagename']     = 'page name';
+$lang['sortkey_o_id']           = 'page ID';
+$lang['sortkey_o_title']        = 'title';
+
+$lang['sortorder']              = 'sort order';
+$lang['sortorder_o_ascending']  = 'ascending';
+$lang['sortorder_o_descending'] = 'descending';
+
+$lang['excluded_pages']         = 'exclude certain pages from blog listing (regular expression required)';
+
+$lang['showhistogram']         = 'Display histogram on archive rendering';
+$lang['max_months']             = 'Max months to show in the histogram';
+$lang['histogram_height']       = 'Height of the histogram (in pixels)';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
