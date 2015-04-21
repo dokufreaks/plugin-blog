caff85760c86e7a3e2225178926ebedea1f995df
Mohammad Anini <mohd.anini@gmail.com>
2015-04-22 00:40:41 +0300

    Arabic Settings Translations

--- lang/ar/settings.php
+++ lang/ar/settings.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * Arabic language file
  *
@@ -13,11 +13,23 @@ $lang['formposition']          = 'موقع التدوينة الجديدة';
 $lang['formposition_o_top']    = 'أعلى';
 $lang['formposition_o_bottom'] = 'أسفل';
 
+$lang['dateprefix']            = 'بادئة التاريخ للتدوينات الجديدة';
+
 $lang['sortkey']               = 'ترتيب التدوينات حسب:';
 $lang['sortkey_o_cdate']       = 'تاريخ الإنشاء';
 $lang['sortkey_o_pagename']    = 'إسم الصفحة';
 $lang['sortkey_o_id']          = 'رقم الصفحة';
 
-$lang['dateprefix']            = 'بادئة التاريخ للتدوينات الجديدة';
+$lang['sortkey_o_mdate']        = 'تاريخ التعديل';
+$lang['sortkey_o_title']        = 'العنوان';
 
+$lang['sortorder']              = 'ترتيباً';
+$lang['sortorder_o_ascending']  = 'تصاعدياً';
+$lang['sortorder_o_descending'] = 'تنازلياً';
+
+$lang['excluded_pages']         = 'استثناء صفحات معيّنة من قوائم المدوّنات (يتطلّب تعبيراً نمطيّاً RegEx)';
+
+$lang['showhistorgram']         = 'عرض الرسم البياني عند عرض الأرشيف';
+$lang['max_months']             = 'العدد الأقصى للأشهر المعروضة في الرسم البياني';
+$lang['histogram_height']       = 'طول الرسم البياني (بكسل)';
 //Setup VIM: ex: et ts=2 enc=utf-8 :
