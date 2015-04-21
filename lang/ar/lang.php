d8fbcf12609f7dba17d012558ec325ae38449808
Mohammad Anini <mohd.anini@gmail.com>
2015-04-22 00:28:13 +0300

    Arabic Translations

--- lang/ar/lang.php
+++ lang/ar/lang.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * Arabic language file
  *
@@ -12,4 +12,25 @@ $lang['older']      = 'تدوينات قديمة';
 $lang['newer']      = 'تدوينات جديدة';
 $lang['newentry']   = 'إضافة تدوينة جديدة:';
 
+$lang['missing_includeplugin']  = 'إضافة "Include" يجب أن تكون منزلّة ومفعّلة لتعمل المدونّة.';
+$lang['missing_pagelistplugin'] = 'إضافة "Pagelist" يجب أن تكون منزلّة ومفعّلة لتعمل قوائم المدوّنات.';
+$lang['missing_tagplugin']      = 'إضافة "Tag" يجب أن تكون منزلّة ومفعّلة لتعمل سحابة الوسوم.';
+
+$lang['autoarchive'] = '%d مشاركة لشهر %s %s';
+$lang['month_01'] = 'كانون الثاني';
+$lang['month_02'] = 'شباط';
+$lang['month_03'] = 'آذار';
+$lang['month_04'] = 'نيسان';
+$lang['month_05'] = 'أيار';
+$lang['month_06'] = 'حزيران';
+$lang['month_07'] = 'تموز';
+$lang['month_08'] = 'آب';
+$lang['month_09'] = 'أيلول';
+$lang['month_10'] = 'تشرين الأول';
+$lang['month_11'] = 'تشرين الثاني';
+$lang['month_12'] = 'كانون الأول';
+
+$lang['entries']       = 'مُدخلات';
+$lang['entry']         = 'مُدخلة';
+$lang['archive_title'] = 'أرشيف المدوّنة';
 //Setup VIM: ex: et ts=2 enc=utf-8 :
