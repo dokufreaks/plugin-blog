a8257b19b93c40abdc3c6163a4664a75b5cccefa
İlker R. Kapaç <irifat@gmail.com>
2015-06-04 12:35:06 +0200

    translation update

---
+++ lang/tr/lang.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author İlker R. Kapaç <irifat@gmail.com>
+ */
+$lang['blog']                  = 'Günce';
+$lang['older']                 = 'Daha eski girdiler';
+$lang['newer']                 = 'Daha yeni girdiler';
+$lang['newentry']              = 'Yeni günce oluştur:';
+$lang['missing_includeplugin'] = 'Güncenin (blog) çalışması için Include eklentisi yüklü olmalıdır.';
+$lang['missing_pagelistplugin'] = 'Arşiv listelerinin çalışması için Pagelist eklentisi yüklü olmalıdır.';
+$lang['missing_tagplugin']     = 'Etiket tanımlayabilmek için Tag eklentisi yüklü olmalıdır.';
+$lang['autoarchive']           = 'Her %2$s %3$s için %1$d Gönderi(ler)';
+$lang['month_01']              = 'Ocak';
+$lang['month_02']              = 'Şubat';
+$lang['month_03']              = 'Mart';
+$lang['month_04']              = 'Nisan';
+$lang['month_05']              = 'Mayıs';
+$lang['month_06']              = 'Haziran';
+$lang['month_07']              = 'Temmuz';
+$lang['month_08']              = 'Ağustos';
+$lang['month_09']              = 'Eylül';
+$lang['month_10']              = 'Ekim';
+$lang['month_11']              = 'Kasım';
+$lang['month_12']              = 'Aralık';
+$lang['entries']               = 'girdiler';
+$lang['entry']                 = 'girdi';
+$lang['archive_title']         = 'Günce tarihçesi';
---
+++ lang/tr/settings.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author İlker R. Kapaç <irifat@gmail.com>
+ */
+$lang['namespace']             = 'günce için varsayılan isimalanı';
+$lang['formposition']          = 'yeni girdi formunun konumu';
+$lang['formposition_o_top']    = 'üst';
+$lang['formposition_o_bottom'] = 'alt';
+$lang['formposition_o_none']   = 'yok';
+$lang['newentrytitle']         = 'yeni girdi formunun başlığı';
+$lang['dateprefix']            = 'yeni girdi kimliklerinde ön ek olarak kullanılacak tarih';
+$lang['sortkey']               = 'günce girdilerinin sıralanma şekli';
+$lang['sortkey_o_cdate']       = 'oluşturma tarihi';
+$lang['sortkey_o_mdate']       = 'değiştirilme tarihi';
+$lang['sortkey_o_pagename']    = 'sayfa adı';
+$lang['sortkey_o_id']          = 'sayfa kimliği';
+$lang['sortkey_o_title']       = 'başlık';
+$lang['sortorder']             = 'sıralama şekli';
+$lang['sortorder_o_ascending'] = 'artan';
+$lang['sortorder_o_descending'] = 'azalan';
+$lang['excluded_pages']        = 'bazı sayfaları günce listesinin dışında tut (kurallı tanımlama gereklidir)';
+$lang['showhistogram']         = 'Arşivi sunarken grafik göster';
+$lang['max_months']            = 'Grafikte gösterilecek en fazla ay sayısı';
+$lang['histogram_height']      = 'Grafiğin yüksekliği (piksel olarak)';
