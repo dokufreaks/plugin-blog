5227dbe9115dc067da5d0f52e40347446d8f969b
Sam01 <m.sajad079@gmail.com>
2016-08-23 07:20:22 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['blog']                  = 'وبلاگ';
+$lang['older']                 = 'نوشته‌های قدیمی‌تر';
+$lang['newer']                 = 'نوشته‌های جدیدتر';
+$lang['newentry']              = 'ثبت وبلاگ جدید:';
+$lang['missing_includeplugin'] = 'برای کارکردن وبلاگ نیاز به نصب پلاگین است.';
+$lang['missing_pagelistplugin'] = 'پلاگینPagelist باید برای آرشیوبندی نصب شود.';
+$lang['missing_tagplugin']     = 'پلاگین برچسب باید نصب شود تا اصطلاحات آن استفاده شود.';
+$lang['autoarchive']           = '%1$d مطلب (s) برای %2$s %3$s';
+$lang['month_01']              = 'ژانویه';
+$lang['month_02']              = 'فوریه';
+$lang['month_03']              = 'مارس';
+$lang['month_04']              = 'آوریل';
+$lang['month_05']              = 'می';
+$lang['month_06']              = 'ژوئن';
+$lang['month_07']              = 'جولای';
+$lang['month_08']              = 'اوت';
+$lang['month_09']              = 'سپتامبر';
+$lang['month_10']              = 'اکتبر';
+$lang['month_11']              = 'نوامبر';
+$lang['month_12']              = 'دسامبر';
+$lang['entries']               = 'ورودی‌ها';
+$lang['entry']                 = 'ورودی';
+$lang['archive_title']         = 'تاریخچه وبلاگ';
---
+++ lang/fa/settings.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['namespace']             = 'فضای نام پیش‌فرض برای وبلاگ';
+$lang['formposition']          = 'موقعیت فرم جدید وارد شده';
+$lang['formposition_o_top']    = 'بالا';
+$lang['formposition_o_bottom'] = 'پایین';
+$lang['formposition_o_none']   = 'هیچ';
+$lang['newentrytitle']         = 'عنوان فرم جدید وارد شده';
+$lang['dateprefix']            = 'تاریخ پیشوند آیدی‌های ورودی‌های جدید';
+$lang['sortkey']               = 'مرتب سازی مطالب وبلاگ براساس';
+$lang['sortkey_o_cdate']       = 'تاریخ ایجاد';
+$lang['sortkey_o_mdate']       = 'تاریخ اصلاح';
+$lang['sortkey_o_pagename']    = 'نام صفحه';
+$lang['sortkey_o_id']          = 'آیدی صفحه';
+$lang['sortkey_o_title']       = 'عنوان';
+$lang['sortorder']             = 'مرتب سازی سفارشی';
+$lang['sortorder_o_ascending'] = 'صعودی';
+$lang['sortorder_o_descending'] = 'نزولی';
+$lang['excluded_pages']        = 'به غیر از صفحه‌های خاص از لیست وبلاگ (عبارت منظم مورد نیاز است)';
+$lang['showhistogram']         = 'نمایش بافت‌نگار در تفسیر آرشیو';
+$lang['max_months']            = 'حداکثر ماه‌ها برای نمایش بافت‌نگار';
+$lang['histogram_height']      = 'ارتفاع بافت‌نگار (به پیکسل)';
