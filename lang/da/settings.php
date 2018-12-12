ba563fa81504d2430ab54e9c3eecee992260b57a
Jacob Palm <mail@jacobpalm.dk>
2018-12-12 20:01:02 +0100

    translation update

--- lang/da/settings.php
+++ lang/da/settings.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Jacob Palm <mail@jacobpalm.dk>
  * @author Esther Brunner <wikidesign@gmail.com>
  * @author Soren Birk <soer9648@eucl.dk>
  */
@@ -11,6 +12,7 @@ $lang['formposition']          = 'position af formular til nyt indlæg';
 $lang['formposition_o_top']    = 'top';
 $lang['formposition_o_bottom'] = 'bund';
 $lang['formposition_o_none']   = 'intet';
+$lang['newentrytitle']         = 'titel på den nye indtastningsformular';
 $lang['dateprefix']            = 'foruddefineret dato for nye indlæg IDs';
 $lang['sortkey']               = 'sortér blog indlæg efter';
 $lang['sortkey_o_cdate']       = 'dato for oprettelse';
@@ -22,3 +24,6 @@ $lang['sortorder']             = 'sorteringsrækkefølge';
 $lang['sortorder_o_ascending'] = 'stigende';
 $lang['sortorder_o_descending'] = 'faldende';
 $lang['excluded_pages']        = 'Udeluk bestemte sider fra blog listen (kræver regulært udtryk)';
+$lang['showhistogram']         = 'Vis histogram ved dannelse af arkiv';
+$lang['max_months']            = 'Maks antal måneder der skal vises i histogram';
+$lang['histogram_height']      = 'Højde på histogram (angiv i pixels)';
