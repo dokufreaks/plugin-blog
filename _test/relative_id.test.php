3e7c1e76a0147cd79fc57d19939c07676cf963c7
Michael Hamann <michael@content-space.de>
2019-08-05 12:12:47 +0200

    Add test cases for resolving relative ids

--- _test/relative_id.test.php
+++ _test/relative_id.test.php
@@ -14,6 +14,32 @@ class plugin_blog_relative_id_test extends DokuWikiTest {
     }
 
     public function test_simple_name_is_absolute() {
+        saveWikiText('blog:start', '{{blog>wiki}}', 'Created blog for wiki ns');
+        $html = p_wiki_xhtml('blog:start');
+        $this->assertContains('Formatting Syntax', $html);
+    }
+
+    public function test_relative_blog() {
+        saveWikiText('blog:start', '{{blog>..:wiki}}', 'Created blog for wiki ns');
+        $html = p_wiki_xhtml('blog:start');
+        $this->assertContains('Formatting Syntax', $html);
+    }
+
+    public function test_absolute_blog() {
+        saveWikiText('blog:start', '{{blog>:wiki}}', 'Created blog for wiki ns');
+        $html = p_wiki_xhtml('blog:start');
+        $this->assertContains('Formatting Syntax', $html);
+    }
+
+    public function test_empty_blog() {
+        global $conf;
+        saveWikiText('blog:start', '{{blog>}}', 'Created blog for wiki ns');
+        $html = p_wiki_xhtml('blog:start');
+        $this->assertNotContains('Formatting Syntax', $html);
+
+        $conf['plugin']['blog']['namespace'] = 'wiki';
+        $html = p_wiki_xhtml('blog:start');
+        $this->assertContains('Formatting Syntax', $html);
 
     }
 }
