58cc20f9c07758e413c625f153cb8228c981ef0b
Michael Hamann <michael@content-space.de>
2019-08-06 14:06:55 +0200

    Use separately created blog content page for test
    This works around a problem in DokuWiki with rendering the syntax page
    with PHP > 7.1, see https://github.com/splitbrain/dokuwiki/issues/2563

--- _test/relative_id.test.php
+++ _test/relative_id.test.php
@@ -7,39 +7,42 @@
  * @group plugins
  */
 class plugin_blog_relative_id_test extends DokuWikiTest {
+    private $blog_content = 'waeWa2oh';
+
     public function setup() {
         $this->pluginsEnabled[] = 'include';
         $this->pluginsEnabled[] = 'blog';
         parent::setup();
+        saveWikiText('blog_content:test', $this->blog_content, 'Create test page with random string for blog test');
     }
 
     public function test_simple_name_is_absolute() {
-        saveWikiText('blog:start', '{{blog>wiki}}', 'Created blog for wiki ns');
+        saveWikiText('blog:start', '{{blog>blog_content}}', 'Created blog for wiki ns');
         $html = p_wiki_xhtml('blog:start');
-        $this->assertContains('Formatting Syntax', $html);
+        $this->assertContains($this->blog_content, $html);
     }
 
     public function test_relative_blog() {
-        saveWikiText('blog:start', '{{blog>..:wiki}}', 'Created blog for wiki ns');
+        saveWikiText('blog:start', '{{blog>..:blog_content}}', 'Created blog for wiki ns');
         $html = p_wiki_xhtml('blog:start');
-        $this->assertContains('Formatting Syntax', $html);
+        $this->assertContains($this->blog_content, $html);
     }
 
     public function test_absolute_blog() {
-        saveWikiText('blog:start', '{{blog>:wiki}}', 'Created blog for wiki ns');
+        saveWikiText('blog:start', '{{blog>:blog_content}}', 'Created blog for wiki ns');
         $html = p_wiki_xhtml('blog:start');
-        $this->assertContains('Formatting Syntax', $html);
+        $this->assertContains($this->blog_content, $html);
     }
 
     public function test_empty_blog() {
         global $conf;
         saveWikiText('blog:start', '{{blog>}}', 'Created blog for wiki ns');
         $html = p_wiki_xhtml('blog:start');
-        $this->assertNotContains('Formatting Syntax', $html);
+        $this->assertNotContains($this->blog_content, $html);
 
-        $conf['plugin']['blog']['namespace'] = 'wiki';
+        $conf['plugin']['blog']['namespace'] = 'blog_content';
         $html = p_wiki_xhtml('blog:start');
-        $this->assertContains('Formatting Syntax', $html);
+        $this->assertContains($this->blog_content, $html);
 
     }
 }
