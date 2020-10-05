9b97ac03bd4188a243551798cec54a6b4a80c26c
qezwan <qezwan@gmail.com>
2020-10-06 00:15:25 +0200

    translation update

---
+++ lang/ckb/lang.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['blog']                  = 'بلۆگ';
+$lang['older']                 = 'تێکردنەکانی کۆنتر';
+$lang['newer']                 = 'تێکردنەکانی نوێتر';
+$lang['newentry']              = 'چوونەژوورەوەی بلۆگی نوێ:';
+$lang['missing_includeplugin'] = 'پێوەکراو دەگرێتە خۆ دەبێت دابەزێنرابێت بۆ بلۆگ بۆ کارکردن.';
+$lang['missing_pagelistplugin'] = 'پێوەکراو پێویستە بۆ لیستی ئەرشیف دابمەزرێنێت بۆ کارکردن.';
+$lang['missing_tagplugin']     = 'پێوەکراو تاگ دەبێت دابەزێنرێت بۆ بەکارهێنانی پوختەکردنی تاگ.';
+$lang['autoarchive']           = '%1$d پۆست بۆ %2$s %3$s';
+$lang['month_01']              = 'کانوونی دووەم';
+$lang['month_02']              = 'شوبات';
+$lang['month_03']              = 'ئازار';
+$lang['month_04']              = 'نیسان';
+$lang['month_05']              = 'ئایار';
+$lang['month_06']              = 'حوزەیران';
+$lang['month_07']              = 'تەممووز';
+$lang['month_08']              = 'ئاب';
+$lang['month_09']              = 'ئەیلوول';
+$lang['month_10']              = 'تشرینی یەکەم';
+$lang['month_11']              = 'تشرینی دووەم';
+$lang['month_12']              = 'کانوونی یەکەم';
+$lang['entries']               = 'تێکردنەکان';
+$lang['entry']                 = 'تێکردن';
+$lang['archive_title']         = 'مێژووی بلۆگ';
---
+++ lang/ckb/settings.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['namespace']             = 'بۆشایی ناوی ئاسایی بۆ بلۆگ';
+$lang['formposition']          = 'شوێنی فۆرمی چوونەناوی نوێ';
+$lang['formposition_o_top']    = 'سەرەوە';
+$lang['formposition_o_bottom'] = 'خوارەوە';
+$lang['formposition_o_none']   = 'هیچ';
+$lang['newentrytitle']         = 'ناونیشانی فۆرمی چوونەژوورەوەی نوێ';
+$lang['dateprefix']            = 'بەرواری پێشگر بۆ ئای دیی تێنووسێکی نوێ';
+$lang['sortkey']               = 'پۆلێنکردنی بڵاگ ەکان بەپێی
+';
+$lang['sortkey_o_cdate']       = 'بەرواری دروست کردن
+';
+$lang['sortkey_o_mdate']       = 'بەرواری هەموارکردن
+';
+$lang['sortkey_o_pagename']    = 'ناوی پەڕە
+';
+$lang['sortkey_o_id']          = 'ناسنامەی پەڕە
+';
+$lang['sortkey_o_title']       = 'ناونیشان
+';
+$lang['sortorder']             = 'ڕیزکردن لە ریزبەند
+';
+$lang['sortorder_o_ascending'] = 'بەرەو سەرەوە
+';
+$lang['sortorder_o_descending'] = 'descending';
+$lang['excluded_pages']        = 'دەرکردنی هەندێک لاپەڕە لە لیستی بلۆگ (دەربڕینی ئاسایی پێویستە)
+';
+$lang['showhistogram']         = 'پیشاندانی هێڵکاری لەسەر نمایشی ئەرشیف
+';
+$lang['max_months']            = 'زۆرینەی مانگ بۆ ئەوەی لە هیستوگرامەکە نیشان بدا
+
+';
+$lang['histogram_height']      = '(بەرزی هیتۆگرام (بە پیکسڵ
+
+';
