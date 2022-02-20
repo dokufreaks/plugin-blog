05e6fa685dc294e5daaab7196fd49fbc5aa3c3ed
Igor Korcek <korcek.igor@gmail.com>
2022-02-20 15:08:45 +0100

    slovak language added

---
+++ lang/sk/settings.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Igor Korček <korcek dot igor at gmail dot com>
+ */
+$lang['namespace']             = 'implicitný menný priestor pre blog';
+
+$lang['formposition']          = 'pozícia poľa pre zadávanie nového príspevku';
+$lang['formposition_o_top']    = 'na vrchu';
+$lang['formposition_o_bottom'] = 'dole';
+$lang['formposition_o_none']   = 'žiadny';
+
+$lang['newentrytitle']          = 'Titulka na stránke pre nový článok';
+
+$lang['dateprefix']            = 'pridať datumový prefix novým príspevkom';
+
+$lang['sortkey']               = 'radenie článkov podľa';
+$lang['sortkey_o_cdate']       = 'dátumu vytvorenia';
+$lang['sortkey_o_mdate']       = 'dátumu úpravy';
+$lang['sortkey_o_pagename']    = 'názvu stránky';
+$lang['sortkey_o_id']          = 'ID stránky';
+$lang['sortkey_o_title']       = 'názov';
+
+$lang['sortorder']             = 'poradie';
+$lang['sortorder_o_ascending'] = 'vzostupné';
+$lang['sortorder_o_descending'] = 'zostupné';
+
+$lang['excluded_pages']         = 'vyčleniť určité stránky zo zobrazovanie v blogu (vyžaduje sa RegEx)';
+
+$lang['showhistogram']         = 'Zobraziť histogram na stránke archívu';
+$lang['max_months']            = 'Maximálny počet mesiacov zobrazených v histogramu';
+$lang['histogram_height']      = 'Výška histogramu (v pixeloch)';
