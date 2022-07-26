edceaf64859bf6ead25228bf3e787af30ef0ac37
Marc Zulet <marczulet@gmail.com>
2022-07-26 12:35:15 +0200

    translation update

---
+++ lang/ca/settings.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Marc Zulet <marczulet@gmail.com>
+ */
+$lang['namespace']             = 'namespace predeterminat per al bloc';
+$lang['formposition']          = 'posició del formulari de nova entrada';
+$lang['formposition_o_top']    = 'amunt';
+$lang['formposition_o_bottom'] = 'avall';
+$lang['formposition_o_none']   = 'cap';
+$lang['newentrytitle']         = 'títol del formulari de nova entrada';
+$lang['dateprefix']            = 'afegir un prefix de data a les publicacions noves';
+$lang['sortkey']               = 'ordenar entrades per';
+$lang['sortkey_o_cdate']       = 'data de creació';
+$lang['sortkey_o_mdate']       = 'data de modificació';
+$lang['sortkey_o_pagename']    = 'nom de la pàgina';
+$lang['sortkey_o_id']          = 'ID de la pàgina';
+$lang['sortkey_o_title']       = 'títol';
+$lang['sortorder']             = 'ordre';
+$lang['sortorder_o_ascending'] = 'ascendent';
+$lang['sortorder_o_descending'] = 'descendent';
+$lang['excluded_pages']        = 'exclou determinades pàgines de la llista de blocs (es requereix una expressió regular)';
+$lang['showhistogram']         = 'Mostra l\'histograma en la representació de l\'arxiu';
+$lang['max_months']            = 'Màxim de mesos per mostrar a l\'histograma';
+$lang['histogram_height']      = 'Alçada de l\'histograma (en píxels)';
