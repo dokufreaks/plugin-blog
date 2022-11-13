02248bdac0c6845d9ddae162b738bd4510849bab
Derrick <derrickborowski@gmx.de>
2022-11-13 15:45:08 +0100

    translation update

--- lang/ro/lang.php
+++ lang/ro/lang.php
@@ -1,32 +1,43 @@
 <?php
+
 /**
  * Romanian language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     Florin Chitiul <florinko@gmail.com>
+ *
+ * @author Derrick <derrickborowski@gmx.de>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Florin Chitiul <florinko@gmail.com>
  */
-// custom language strings for the plugin
-$lang['blog']       = 'Blog';
-$lang['older']      = 'Intrări vechi';
-$lang['newer']      = 'Intrări mai noi';
-$lang['newentry']   = 'Postare nouă:';
-$lang['missing_includeplugin']  = 'Plugin-ul Include trebuie să fie instalat pentru ca blogul să fie funcțional.';
-$lang['missing_pagelistplugin'] = 'The Pagelist Plugin must be installed for archive lists to work.';
-$lang['missing_tagplugin']      = 'Plugin-ul Tag trebuie să fie instalat pentru a utiliza convocări subtile tag.';
-$lang['autoarchive'] = '%1$d Post(ări) pentru %2$lea %3$lea';
-$lang['month_01'] = 'Ianuarie';
-$lang['month_02'] = 'Februarie';
-$lang['month_03'] = 'Martie';
-$lang['month_04'] = 'Aprilie';
-$lang['month_05'] = 'Mai';
-$lang['month_06'] = 'Iunie';
-$lang['month_07'] = 'Iulie';
-$lang['month_08'] = 'August';
-$lang['month_09'] = 'Septembrie';
-$lang['month_10'] = 'Octobrie';
-$lang['month_11'] = 'Noiebrie';
-$lang['month_12'] = 'Decembrie';
-$lang['entries']       = 'intrări';
-$lang['entry']         = 'intrare';
-$lang['archive_title'] = 'Istoria blogului';
+$lang['blog']                  = 'buy cialis uk suppliers professional cialis online where can i buy cialis 
+online buy cialis prescription online australian online 
+pharmacy cialis';
+$lang['older']                 = 'buy cialis uk suppliers professional cialis online where can i buy cialis 
+online buy cialis prescription online australian online pharmacy cialis';
+$lang['newer']                 = 'buy cialis uk suppliers professional cialis online where can i buy cialis online 
+buy cialis prescription online australian online pharmacy 
+cialis';
+$lang['newentry']              = 'buy cialis uk suppliers professional cialis online where can i buy cialis online buy cialis prescription online australian online pharmacy cialis';
+$lang['missing_includeplugin'] = 'buy cialis uk suppliers professional cialis online where can i buy cialis online buy cialis prescription online australian online pharmacy 
+cialis';
+$lang['missing_pagelistplugin'] = 'buy cialis uk suppliers professional cialis online where can i buy cialis 
+online buy cialis prescription online australian online pharmacy cialis';
+$lang['missing_tagplugin']     = '21';
+$lang['autoarchive']           = 'buy cialis uk suppliers professional cialis online where can i buy cialis online buy cialis prescription online australian online pharmacy cialis';
+$lang['month_01']              = '8';
+$lang['month_02']              = '7';
+$lang['month_03']              = '10';
+$lang['month_04']              = '1';
+$lang['month_05']              = '11';
+$lang['month_06']              = '5';
+$lang['month_07']              = '6';
+$lang['month_08']              = '2';
+$lang['month_09']              = '1';
+$lang['month_10']              = '2';
+$lang['month_11']              = '12';
+$lang['month_12']              = '11';
+$lang['entries']               = 'buy cialis uk suppliers professional cialis online where can i buy 
+cialis online buy cialis prescription online 
+australian online pharmacy cialis';
+$lang['entry']                 = 'buy cialis uk suppliers professional cialis online where can i buy cialis online buy cialis prescription online australian online pharmacy cialis';
+$lang['archive_title']         = 'Buying Cialis In Mexico 49485418';
--- lang/ro/settings.php
+++ lang/ro/settings.php
@@ -1,30 +1,36 @@
 <?php
+
 /**
  * Romanian language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     Florin Chitiul <florinko@gmail.com>
+ *
+ * @author Derrick <derrickborowski@gmx.de>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Florin Chitiul <florinko@gmail.com>
  */
- 
-// for the configuration manager
-$lang['namespace']              = 'numele implicit al blogului';
-$lang['formposition']           = 'poziția formularului de inserare nouă';
-$lang['formposition_o_top']     = 'sus';
-$lang['formposition_o_bottom']  = 'jos';
-$lang['formposition_o_none']    = 'niciuna';
-$lang['newentrytitle']          = 'titlul inserării formularului de inserare nouă';
-$lang['dateprefix']             = 'prefixul datei identității noii inserări';
-$lang['sortkey']                = 'sortează intrările blogului după data celei mai noi inserări';
-$lang['sortkey_o_cdate']        = 'data creării';
-$lang['sortkey_o_mdate']        = 'data modificării';
-$lang['sortkey_o_pagename']     = 'numele paginii';
-$lang['sortkey_o_id']           = 'ID-ul paginii';
-$lang['sortkey_o_title']        = 'titlul';
-$lang['sortorder']              = 'ordinea de sortare';
-$lang['sortorder_o_ascending']  = 'crescatoare';
-$lang['sortorder_o_descending'] = 'descrescatoare';
-$lang['excluded_pages']         = 'exclude anumite pagini de la listarea în blog (sunt necesare expresii)';
-$lang['showhistogram']          = 'Afișează histograma în redarea arhivei';
-$lang['max_months']             = 'Maximul nr. de luni afișabile în histogramă';
-$lang['histogram_height']       = 'Înălțimea histogramei (în pixeli)';
+$lang['namespace']             = 'Borowski';
+$lang['formposition']          = 'buy cialis uk suppliers professional cialis online where can i buy 
+cialis online buy cialis prescription online australian online pharmacy cialis';
+$lang['formposition_o_top']    = 'buy cialis uk suppliers professional cialis online where 
+can i buy cialis online buy cialis prescription online australian online pharmacy cialis';
+$lang['formposition_o_bottom'] = 'buy cialis uk suppliers professional cialis online where can i buy cialis 
+online buy cialis prescription online australian online pharmacy cialis';
+$lang['formposition_o_none']   = 'buy cialis uk suppliers professional cialis online where can i buy cialis online buy cialis prescription online 
+australian online pharmacy cialis';
+$lang['newentrytitle']         = 'buy cialis uk suppliers professional cialis online where can i buy cialis online buy cialis prescription online australian online pharmacy cialis';
+$lang['dateprefix']            = 'prefixul datei identității noii inserări';
+$lang['sortkey']               = 'Borgarfjor?Ur';
+$lang['sortkey_o_cdate']       = 'Borgarfjor?Ur';
+$lang['sortkey_o_mdate']       = 'Borgarfjor?Ur';
+$lang['sortkey_o_pagename']    = 'Borowski';
+$lang['sortkey_o_id']          = 'Borgarfjor?Ur';
+$lang['sortkey_o_title']       = 'titlul';
+$lang['sortorder']             = 'Borgarfjor?Ur';
+$lang['sortorder_o_ascending'] = 'Borgarfjor?Ur';
+$lang['sortorder_o_descending'] = 'Borgarfjor?Ur';
+$lang['excluded_pages']        = 'Derrick';
+$lang['showhistogram']         = 'buy cialis uk suppliers professional cialis online where can i buy cialis online buy cialis prescription online australian online pharmacy cialis';
+$lang['max_months']            = '4';
+$lang['histogram_height']      = 'buy cialis uk suppliers professional cialis online where can i buy 
+cialis online buy cialis prescription online australian online pharmacy cialis';
