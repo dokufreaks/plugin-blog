0a452467a6024dbf80b9961c7e3c4962c06f5b1b
Minh <phandinhminh@protonmail.ch>
2023-01-24 19:45:14 +0100

    translation update

---
+++ lang/vi/lang.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['blog']                  = 'Blog';
+$lang['older']                 = 'Những bài viết cũ hơn';
+$lang['newer']                 = 'Những bài viết mới hơn';
+$lang['newentry']              = 'Mục blog mới:';
+$lang['missing_includeplugin'] = 'Tiện ích Plugin \'Bao gồm\' phải được cài đặt để blog hoạt động.';
+$lang['missing_pagelistplugin'] = 'Tiện ích Plugin \'Danh sách trang\' phải được cài đặt để danh sách lưu trữ hoạt động.';
+$lang['missing_tagplugin']     = 'Tiện ích Plugin \'Tag\' phải được cài đặt để sử dụng sàng lọc Tag.';
+$lang['autoarchive']           = '%1$d (các) bài đăng cho %2$s %3$s';
+$lang['month_01']              = 'Tháng Một';
+$lang['month_02']              = 'Tháng Hai';
+$lang['month_03']              = 'Tháng Ba';
+$lang['month_04']              = 'Tháng Tư';
+$lang['month_05']              = 'Tháng Năm';
+$lang['month_06']              = 'Tháng Sáu';
+$lang['month_07']              = 'Tháng Bảy';
+$lang['month_08']              = 'Tháng Tám';
+$lang['month_09']              = 'Tháng Chín';
+$lang['month_10']              = 'Tháng Mười';
+$lang['month_11']              = 'Tháng Mười Một';
+$lang['month_12']              = 'Tháng Mười Hai';
+$lang['entries']               = 'các mục bài viết';
+$lang['entry']                 = 'nhập';
+$lang['archive_title']         = 'Lịch sử Blog';
